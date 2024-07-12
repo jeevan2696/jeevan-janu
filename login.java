bc518db0df6b62d2406773971aabad837a2916b3
root <root@ip-172-31-25-205.ap-southeast-1.compute.internal>
2024-07-12 11:33:43 +0000

    newfile added

--- login.java
+++ login.java
@@ -1,1 +1,2 @@
-As per scriptures, Radha is considered as the complete incarnation of Mahalakshmi. It is believed that Krishna enchants the world, but Radha enchants even him. Therefore, she is the supreme goddess of all, and together they are called Radha-Krishna.
+As per scriptures, Radha is considered as the complete incarnation of Mahalakshmi. It is believed that Krishna enchants the world, but Radha enchants even him. Therefore, she is the supreme goddess of all, and together they are called Radha-Krishna
+i love u kannamma.
